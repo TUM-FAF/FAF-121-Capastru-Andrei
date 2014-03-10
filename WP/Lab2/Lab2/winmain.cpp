8c144346fd0e5710dcb1a69ea77772b72c38be54
Andrew <andreicap@me.com>
2014-03-10 20:45:59 +0200

    Lab2 update: Added ScrollBars and Menus

--- WP/Lab2/Lab2/winmain.cpp
+++ WP/Lab2/Lab2/winmain.cpp
@@ -5,14 +5,15 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #include <windows.h>
 #include <stdlib.h>
-
-#define SCROLL_1 100
+#include <string>
+#include "resources.h"
 
 LRESULT CALLBACK WinProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst,LPSTR lpCmdLine,int nShowCmd)
 {
 	WNDCLASSEX winClass;
+
 	ZeroMemory(&winClass,sizeof(WNDCLASSEX));
 	winClass.cbClsExtra=NULL;
 	winClass.cbSize=sizeof(WNDCLASSEX);
@@ -36,13 +37,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst,LPSTR lpCmdLine,int 
 			MB_ICONERROR);
 	}
 
+	//receive screen dimensions
 	int screenHeight = GetSystemMetrics(SM_CYSCREEN); 
 	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
 
 	HWND hWnd=CreateWindowEx(NULL,
 			"Window Class",
-			"Lab#1",
-			WS_OVERLAPPEDWINDOW |WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX,
+			"Lab#2",
+			WS_OVERLAPPEDWINDOW| 
+			WS_HSCROLL | WS_VSCROLL,
 			screenWidth/6,		//leftx
 			screenHeight/7,		//lefty
 			screenWidth - 2*screenWidth/6, //rightx
@@ -65,6 +68,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst,LPSTR lpCmdLine,int 
 
 	MSG msg;
 	ZeroMemory(&msg,sizeof(MSG));
+
 	while(GetMessage(&msg,NULL,0,0))
 	{
 		TranslateMessage(&msg);
@@ -75,9 +79,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst,LPSTR lpCmdLine,int 
 
 LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
 {
+	static int  iColorID[5] = { 
+		WHITE_BRUSH,  LTGRAY_BRUSH, GRAY_BRUSH,
+		DKGRAY_BRUSH, BLACK_BRUSH 
+	};
+     static int  iSelection = IDM_WHITE ;
+    static HMENU  hMenu;
+
 	switch(msg)
 	{
 
+		//minmaxinfo - setting limits on window dimensions
 		case WM_GETMINMAXINFO:
 		{
 			int screenHeight = GetSystemMetrics(SM_CYSCREEN);
@@ -92,35 +104,67 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
 
 		case WM_CREATE:
 		{
-			HINSTANCE hInst = (HINSTANCE) GetWindowLong(hWnd, GWL_HINSTANCE);
-			RECT rect;
-			GetClientRect(hWnd, &rect);
-			HWND hScroll =    CreateWindowEx(0L,
-                       "SCROLLBAR",
-                       NULL, // There is no text to display
-                       WS_CHILD | WS_VISIBLE,
-					   rect.right-40,
-                       rect.top,
-					   rect.right-20,
-					   rect.bottom-20,
-                       hWnd,
-                       (HMENU)SCROLL_1,
-                       hInst,
-                       NULL);
-
-			SetScrollRange(hScroll, SB_CTL , 0, 255, false);
-			SetScrollPos(hScroll, SB_CTL , 0, FALSE);
-
+			SetScrollRange(hWnd, SB_VERT, 0, 100, FALSE);
+			
+			
 		}
 		break;
 		//end wm_create
 
-		case WM_COMMAND:
+		case WM_VSCROLL:
 		{
 			switch(LOWORD(wParam))
-            {
+			{
+			case SB_THUMBPOSITION:
+				{
+					SetScrollPos(hWnd, SB_VERT, 50, FALSE); 
+				}break;
+				
+			}
+		}break;
+		//end wm_vscroll
+		case WM_COMMAND:
+		{
+			hMenu = GetMenu (hWnd) ;
 
-			}break;
+			switch (LOWORD (wParam))
+				{
+				case IDM_RESTART :
+						MessageBeep (0) ;
+						return 0 ;
+
+				case IDM_EXIT :
+						SendMessage (hWnd, WM_CLOSE, 0, 0L) ;
+						return 0 ;
+
+				case IDM_WHITE :          // Note: Logic below
+				case IDM_LTGRAY :         //   assumes that IDM_WHITE
+				case IDM_GRAY :           //   through IDM_BLACK are
+				case IDM_DKGRAY :         //   consecutive numbers in
+				case IDM_BLACK :          //   the order shown here.
+
+						CheckMenuItem (hMenu, iSelection, MF_UNCHECKED) ;
+						iSelection = LOWORD (wParam) ;
+						CheckMenuItem (hMenu, iSelection, MF_CHECKED) ;
+     
+						SetClassLong (hWnd, GCL_HBRBACKGROUND,
+							(LONG) GetStockObject 
+								(iColorID[LOWORD (wParam) - IDM_WHITE])) ;
+
+						InvalidateRect (hWnd, NULL, TRUE) ;
+						return 0 ;
+
+				case IDM_HELP :
+						MessageBox (hWnd, "Help not yet implemented!",
+							"Lab#2", MB_ICONEXCLAMATION | MB_OK) ;
+						return 0 ;
+
+				case IDM_ABOUT :
+						MessageBox (hWnd, "Menu Demonstration Program.",
+							"Lab#2", MB_ICONINFORMATION | MB_OK) ;
+						return 0 ;
+			}
+			//end switch()
 		}break;
 		//end wm_command
 
