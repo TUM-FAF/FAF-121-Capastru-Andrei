61e1439800b863edad4880b910b36722f2d4bb31
Andrew <andreicap@me.com>
2014-03-10 20:46:24 +0200

    Lab2 update on Menus

---
+++ WP/Lab2/Lab2/resources.h
@@ -0,0 +1,14 @@
+#define IDC_MAINSCROLL 100 
+
+#define IDM_RESTART  1
+#define IDM_EXIT     2
+
+#define IDM_WHITE   10
+#define IDM_LTGRAY  11
+#define IDM_GRAY    12
+#define IDM_DKGRAY  13
+#define IDM_BLACK   14
+#define IDM_RANDOM  15
+
+#define IDM_HELP    40
+#define IDM_ABOUT   41
