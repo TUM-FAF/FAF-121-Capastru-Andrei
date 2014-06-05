d93702bbca8730522617bad7019a6d9785f899bd
andrewcap <andrei.capastru@gmail.com>
2014-06-05 03:00:39 +0300

    innitial commit lab2IDE

---
+++ WP/Lab3/Lab3/resource.h
@@ -0,0 +1,23 @@
+
+#define ID_CLEAR  101
+#define ID_CURSOR 102
+#define ID_RGB 103
+#define ID_EXIT 104
+#define ID_DEFAULT 106
+#define ID_WIDTH 107
+#define ID_LINE 108
+#define ID_STATIC 109
+#define ID_RECT 111
+#define ID_CIRCLE 112
+#define ID_ELIPSE 110
+#define BUTTON_STATIC 121
+#define ID_PEN 120
+#define ID_R 122
+#define ID_G 123
+#define ID_B 124
+#define ID_R1 125
+#define ID_G1 126
+#define ID_B1 127
+#define ID_FILL 130
+
+
