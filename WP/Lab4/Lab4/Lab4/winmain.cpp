69da63616fd6e3f6f5609d6998f3727cd09a2b3b
andrewcap <andrei.capastru@gmail.com>
2014-06-18 17:40:09 +0300

    Added acceleration with mouse wheel Lab4 WP

--- WP/Lab4/Lab4/Lab4/winmain.cpp
+++ WP/Lab4/Lab4/Lab4/winmain.cpp
@@ -323,7 +323,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         break;
 		case WM_MOUSEWHEEL:
         {
-			SendMessage(hwnd, WM_TIMER, 0, 0);
+			SendMessage(hwnd, WM_TIMER
         }
         case WM_KEYDOWN:
         {
